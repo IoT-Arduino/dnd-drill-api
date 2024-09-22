24ee301e0a7b80a7c2635ac6e9ba0378d185701e
IoT-Arduino <bunkajin@icloud.com>
2024-09-22 15:30:00 +0900

    Clerk連携（ただしcorsエラー）

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,71 @@ import { Hono } from "hono";
 import { drizzle } from "drizzle-orm/d1";
 import { drills } from "./../db/schema";
 import { eq } from "drizzle-orm";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { bearerAuth } from 'hono/bearer-auth';
+import { clerkClient, createClerkClient } from '@clerk/clerk-sdk-node';
+import { cors } from 'hono/cors';
+
+type Bindings = {
+	DB: D1Database;
+	CLERK_PUBLISHABLE_KEY: string;
+	CLERK_SECRET_KEY: string;
+};
 
 const app = new Hono<{ Bindings: Bindings }>().basePath("/api");
 
-app.get("/drills/:id", async (c) => {
-	const id = parseInt(c.req.param("id"));
-	try {
-		const db = drizzle(c.env.DB);
-		const results = await db.select().from(drills).where(eq(drills.id, id));
-		return c.json(results);
-	} catch (e) {
-		return c.json({ err: e }, 500);
+app.use('/api/*', cors({
+	origin: ['http://localhost:5173'], // Reactアプリのオリジン
+	allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+	allowHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
+	exposeHeaders: ['Content-Length'],
+	maxAge: 0,// preflight requestのキャッシュ時間	
+	credentials: true,
+}));
+
+// app.use('*', (c, next) => {
+// 	c.header('Access-Control-Allow-Origin', '*')
+// 	c.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+// 	c.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+// 	return next()
+// })
+
+app.options('*', (c) => {
+	return new Response(null, { status: 204 });
+})
+
+
+app.use('*', clerkMiddleware())
+
+
+// 公開エンドポイント
+app.get("/public", (c) => c.text("This is a public endpoint"));
+
+// 認証が必要なエンドポイント
+app.get("/protected", async (c) => {
+
+	const auth = getAuth(c)
+
+	if (!auth?.userId) {
+		return c.json({
+			message: 'You are not logged in.',
+		})
 	}
+
+	return c.json({
+		message: 'You are logged in!',
+		userId: auth.userId,
+	})
 });
 
 app.get("/drills", async (c) => {
+
+	const auth = getAuth(c)
+	if (!auth?.userId) {
+		return c.json({
+			message: 'You are not logged in.',
+		})
+	}
 	try {
 		const db = drizzle(c.env.DB);
 		const results = await db.select().from(drills);
