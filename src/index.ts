20e3a1e1630b70a5f28d6415430be3b99bee145a
IoT-Arduino <bunkajin@icloud.com>
2024-10-05 19:28:20 +0900

    API基本修正（drills, history)

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,35 @@
 import { Hono } from "hono";
 import { drizzle } from "drizzle-orm/d1";
-import { drills,history } from "./../db/schema";
+import { drills, history } from "./../db/schema";
 import { eq, sql } from "drizzle-orm";
 import { cors } from 'hono/cors';
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { CORS_ORIGIN } from "./const";
+import { HistoryEntry } from './types'
 
 type Bindings = {
 	DB: D1Database;
 	CLERK_PUBLISHABLE_KEY: string;
 	CLERK_SECRET_KEY: string;
-	CORS_ORIGIN:string;
+	CORS_ORIGIN: string;
 };
 
 const app = new Hono<{ Bindings: Bindings }>().basePath("/api");
 
 // origin ドメインを明示的に示すもの。はずすとCORSエラーになる。
 app.use('*', async (c, next) => {
-    const corsMiddlewareHandler = cors({
-      origin: c.env.CORS_ORIGIN,
-    })
-    return corsMiddlewareHandler(c, next)
-  })
+	const corsMiddlewareHandler = cors({
+		origin: c.env.CORS_ORIGIN,
+	})
+	return corsMiddlewareHandler(c, next)
+})
 
 app.options('*', (c) => {
 	return new Response(null, { status: 204 });
 })
 
 app.use('/api/*', cors({
-	origin: ['http://localhost:5173'], // Reactアプリのオリジン
+	origin: [CORS_ORIGIN], // Reactアプリのオリジン
 	allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 	allowHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
 	exposeHeaders: [],
@@ -37,6 +39,7 @@ app.use('/api/*', cors({
 
 app.use('*', clerkMiddleware())
 
+// drills api
 app.get("/drills", async (c) => {
 	const auth = getAuth(c)
 	if (!auth?.userId) {
@@ -78,13 +81,25 @@ app.post("/drills", async (c) => {
 			message: 'You are not logged in.',
 		})
 	}
-	const drill = await c.req.json<typeof drills.$inferInsert>()
+	const drillFromClient = await c.req.json<typeof drills.$inferInsert>()
 	try {
 		const db = drizzle(c.env.DB);
-		await db.insert(drills).values(drill);
-		return c.json({ message: "success" }, 201)
-	} catch (e) {
-		return c.json({ err: e }, 500);
+		const drillToInsert = {
+			...drillFromClient,
+			userId: auth.userId // userIdを追加
+		};
+		const [insertedDrill] = await db.insert(drills).values(drillToInsert).returning();
+
+		// JSON形式でレスポンスを返す
+		return new Response(JSON.stringify(insertedDrill), {
+			status: 201,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+	} catch (e: any) {
+		console.error('Error inserting drill:', e);
+		return c.json({ error: e.message, stack: e.stack }, 500);
 	}
 });
 
@@ -97,13 +112,26 @@ app.put("/drills/:id", async (c) => {
 		})
 	}
 	const id = parseInt(c.req.param("id"));
-	const { status, columnId, content } = await c.req.json<typeof drills.$inferInsert>()
+	const { url, content, status, columnId } = await c.req.json<Partial<typeof drills.$inferInsert>>()
+
 	try {
 		const db = drizzle(c.env.DB);
-		await db.update(drills).set({ status, columnId, content }).where(eq(drills.id, id));
-		return c.json({ message: "success" }, 201);
-	} catch (e) {
-		return c.json({ err: e }, 500);
+
+		const [updatedDrill] = await db.update(drills)
+			.set({ url, content, status, columnId })
+			.where(eq(drills.id, id))
+			.returning();
+
+		// JSON形式でレスポンスを返す
+		return new Response(JSON.stringify(updatedDrill), {
+			status: 201,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+	} catch (e: any) {
+		console.error('Error updating drill:', e);
+		return c.json({ error: e.message, stack: e.stack }, 500);
 	}
 });
 
@@ -124,18 +152,7 @@ app.delete("/drills/:id", async (c) => {
 	}
 });
 
-type HistoryEntry = {
-	id: number;
-	userId: string;
-	memo: string;
-	createdAt: string;
-	drills: string;
-  };
-  
-  type GroupedHistory = {
-	[date: string]: HistoryEntry[];
-  };
-
+// history 用 api
 app.get("/history", async (c) => {
 	const auth = getAuth(c)
 	if (!auth?.userId) {
@@ -143,24 +160,11 @@ app.get("/history", async (c) => {
 			message: 'You are not logged in.',
 		})
 	}
-	// console.log("userId",auth.userId)
 	try {
-		// co
 		const db = drizzle(c.env.DB);
 		const results = await db.select()
-		  .from(history)
-		  .orderBy(sql`${history.createdAt} DESC`) as HistoryEntry[];
-	
-		const groupedResults = results.reduce<GroupedHistory>((acc, item) => {
-		  if (!acc[item.createdAt]) {
-			acc[item.createdAt] = {
-			  memo: item.memo,
-			  userId: item.userId,
-			  drills: JSON.parse(item.drills)
-			};
-		  }
-		  return acc;
-		}, {} )
+			.from(history)
+			.orderBy(sql`${history.createdAt} DESC`) as HistoryEntry[];
 
 		return c.json(results);
 	} catch (e) {
@@ -168,8 +172,35 @@ app.get("/history", async (c) => {
 	}
 });
 
+app.post("/history", async (c) => {
+	const auth = getAuth(c)
+	if (!auth?.userId) {
+		return c.json({
+			message: 'You are not logged in.',
+		}, 401)
+	}
 
+	try {
+		const { memo, drillItemsChecked } = await c.req.json();
 
+		console.log("Received data:", { memo, drillItemsChecked });
+		const drillItemsCheckedArray = drillItemsChecked.map((item: { content: string }) => item.content);
+		const db = drizzle(c.env.DB);
+		const newHistoryEntry = {
+			userId: auth.userId,
+			memo,
+			drills: JSON.stringify(drillItemsCheckedArray),
+			createdAt: new Date().toLocaleString('ja-JP', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false })
+		};
+
+		await db.insert(history).values(newHistoryEntry);
+
+		return c.json({ message: "History entry created successfully" }, 201);
+	} catch (e) {
+		console.error('Error creating history entry:', e);
+		return c.json({ error: 'Internal Server Error' }, 500);
+	}
+});
 
 
 
